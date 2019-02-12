c5688fd112b10e70edf97e651bd08ac950380952
13JaviG <32481807+13JaviG@users.noreply.github.com>
2019-02-12 18:27:20 +0100

    Commit Inicial.

---
+++ Laboratorio2/src/laboratorio2/Main.java
@@ -0,0 +1,13 @@
+package laboratorio2;
+
+public class Main {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		Interfaz window = new Interfaz();
+		window.setVisible(true);
+	}
+	
+}
